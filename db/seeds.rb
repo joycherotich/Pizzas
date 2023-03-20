301084a3d0689642454bc5fa97b9c3608c507a0c
joy <letimjoy7@gmail.com>
2023-03-20 15:45:40 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
 # create some restaurants
-sottocasa = Restaurant.create(name: "Sottocasa NYC", address: "298 Atlantic Ave, Brooklyn, NY 11201")
-pizzarte = Restaurant.create(name: "PizzArte", address: "69 W 55th St, New York, NY 10019")
+restaurant = Restaurant.create(name: Faker::Restaurant.name, address: "298 Atlantic Ave, Brooklyn, NY 11201")
+restaurant = Restaurant.create(name: Faker::Restaurant.name, address: "69 W 55th St, New York, NY 10019")
 
 # create some pizzas
 cheese = Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
 pepperoni = Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
 
+# # associate pizzas with restaurants
+# sottocasa.pizzas << cheese
+# sottocasa.pizzas << pepperoni
+# pizzarte.pizzas << cheese
 
-
-# create some restaurant pizzas
-RestaurantPizza.create(price: 10.00, restaurant: sottocasa, pizza: cheese)
-RestaurantPizza.create(price: 12.00, restaurant: sottocasa, pizza: pepperoni)
-RestaurantPizza.create(price: 8.78, restaurant: pizzarte, pizza: cheese)
+# # create some restaurant pizzas
+# RestaurantPizza.create(price: 10.00, restaurant: sottocasa, pizza: cheese)
+# RestaurantPizza.create(price: 12.00, restaurant: sottocasa, pizza: pepperoni)
+# RestaurantPizza.create(price: 8.78, restaurant: pizzarte, pizza: cheese)

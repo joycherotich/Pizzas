974ed7fac91331e5ed1a9cf0a285064c88f94d75
joy <letimjoy7@gmail.com>
2023-03-20 16:13:57 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ restaurant = Restaurant.create(name: Faker::Restaurant.name, address: "298 Atlan
 restaurant = Restaurant.create(name: Faker::Restaurant.name, address: "69 W 55th St, New York, NY 10019")
 
 # create some pizzas
-cheese = Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
+cheese = Pizza.create(name:  "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
 pepperoni = Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
 
 # # associate pizzas with restaurants
